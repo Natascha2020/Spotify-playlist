38c22338fd308e111382dc28b82d3e2525894f02
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-27 15:14:08 +0200

    Integrated FixedHeader component, embedded it in Playlist

--- spotify-playlist/src/Components/Playlist.js
+++ spotify-playlist/src/Components/Playlist.js
@@ -1,10 +1,22 @@
 import React from "react";
 import PlayButton from "./PlayButton";
 import DeleteButton from "./DeleteButton";
+import FixedHeader from "./FixedHeader.js";
 import "./Playlist.css";
 
 //Creating blueprint for song-Layout and Button-Functions
-export default function Playlist({ title, description, genre, author, release, album, image, itemIndex, playHandler, removeHandler }) {
+export default function Playlist({
+  title,
+  description,
+  genre,
+  author,
+  release,
+  album,
+  image,
+  itemIndex,
+  playHandler,
+  removeHandler
+}) {
   //Creating button-events by accessing Song-Data through play-Handler property from parent(PlaylistView) using index of each song
   const playSong = (e, itemIndex) => {
     playHandler(itemIndex);
@@ -16,37 +28,40 @@ export default function Playlist({ title, description, genre, author, release, a
 
   //Importing Play- and Deletebutton and calling respective events (playSong and deleteSong)
   return (
-    <div className="Playlist">
-      <div className="song">
-        <img src={image} className="Image" alt="" />
+    <>
+      <FixedHeader />
+      <div className="Playlist">
+        <div className="song">
+          <img src={image} className="Image" alt="" />
 
-        <div className="text">
-          <h2 className="title">{title}</h2>
-          <h3 className="author">{author}</h3>
+          <div className="text">
+            <h2 className="title">{title}</h2>
+            <h3 className="author">{author}</h3>
 
-          <div className="sideInfo">
-            <span className="genre">{genre}</span>
-            <span className="release">{release}</span>
+            <div className="sideInfo">
+              <span className="genre">{genre}</span>
+              <span className="release">{release}</span>
+            </div>
+            <h4 className="album">{album}</h4>
+            <div className="description">{description}</div>
           </div>
-          <h4 className="album">{album}</h4>
-          <div className="description">{description}</div>
-        </div>
 
-        <div className="buttons">
-          <PlayButton
-            value="Play now"
-            handleClick={(e) => {
-              playSong(e, itemIndex);
-            }}
-          />
-          <DeleteButton
-            value="Delete"
-            handleClick={(e) => {
-              deleteSong(e, itemIndex);
-            }}
-          />
+          <div className="buttons">
+            <PlayButton
+              value="Play now"
+              handleClick={(e) => {
+                playSong(e, itemIndex);
+              }}
+            />
+            <DeleteButton
+              value="Delete"
+              handleClick={(e) => {
+                deleteSong(e, itemIndex);
+              }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
